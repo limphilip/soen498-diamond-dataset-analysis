c10aedd657ca475c7693f4a64986f0ea34498f25
Ideawin <ideawinkoun@gmail.com>
2018-03-31 20:18:15 -0400

    Added all python files for Random Forests

---
+++ machine_learning/predict_cut_subset.py
@@ -0,0 +1,42 @@
+from pyspark.sql import Row, SparkSession
+from pyspark.mllib.tree import RandomForest, RandomForestModel
+from pyspark.mllib.util import MLUtils
+from pyspark.sql import SparkSession
+from pyspark import SparkContext, SparkConf
+
+spark = SparkSession.builder.master("local").appName("DiamondProject").getOrCreate()
+sc = spark.sparkContext
+
+# Load and parse the data file into an RDD of LabeledPoint.
+data = MLUtils.loadLibSVMFile(sc, 'data/diamonds_dimensions_only.data')
+
+# Split the data into training and test sets (30% held out for testing)
+(trainingData, testData) = data.randomSplit([0.7, 0.3],seed=123)
+
+# Train a RandomForest model.
+# Empty categoricalFeaturesInfo indicates all features are continuous.
+# Note: Use larger numTrees in practice.
+# Setting featureSubsetStrategy="auto" lets the algorithm choose.
+model = RandomForest.trainClassifier(trainingData, numClasses=9, categoricalFeaturesInfo={},
+                                     numTrees=25, featureSubsetStrategy="auto",
+                                     impurity='gini', maxDepth=20, maxBins=32, seed=123)
+
+# Evaluate model on test instances and compute test error
+predictions = model.predict(testData.map(lambda x: x.features))
+labelsAndPredictions = testData.map(lambda lp: lp.label).zip(predictions)
+testErr = labelsAndPredictions.filter(
+    lambda lp: lp[0] != lp[1]).count() / float(testData.count())
+result = testData.zip(predictions).collect()
+
+with open('machine_learning/cut_predicted_subset.txt', 'w') as f:
+	for i in result:
+		f.write(i)
+		f.write('Test Error = ' + str(testErr))
+
+labeled_result = labelsAndPredictions.map(lambda p: Row(cut=float(p[0]), predictions=float(p[1])))
+result = spark.createDataFrame(labeled_result).show(25)
+print('Test Error = ' + str(testErr))
+
+# Save and load model
+model.save(sc, "machine_learning/models/DiamondClassificationModel_subset")
+sameModel = RandomForestModel.load(sc, "machine_learning/models/DiamondClassificationModel_subset")
---
+++ machine_learning/predict_price.py
@@ -0,0 +1,38 @@
+from pyspark.sql import Row, SparkSession
+from pyspark.mllib.tree import RandomForest, RandomForestModel
+from pyspark.mllib.util import MLUtils
+from pyspark.sql import SparkSession
+from pyspark import SparkContext, SparkConf
+
+#conf = SparkConf().setAppName("DiamondProject").setMaster("local")
+#sc = SparkContext(conf=conf)
+spark = SparkSession.builder.master("local").appName("DiamondProject").getOrCreate()
+sc = spark.sparkContext
+
+# Load and parse the data file into an RDD of LabeledPoint.
+data = MLUtils.loadLibSVMFile(sc, 'data/diamonds_price.data')
+
+# Split the data into training and test sets (30% held out for testing)
+(trainingData, testData) = data.randomSplit([0.7, 0.3],seed=123)
+
+# Train a RandomForest model.
+#  Empty categoricalFeaturesInfo indicates all features are continuous.
+#  Note: Use larger numTrees in practice.
+#  Setting featureSubsetStrategy="auto" lets the algorithm choose.
+model = RandomForest.trainRegressor(trainingData, categoricalFeaturesInfo={},
+                                     numTrees=20, featureSubsetStrategy="auto",
+                                     impurity='variance', maxDepth=20, maxBins=32, seed=123)
+
+# Evaluate model on test instances and compute test error
+predictions = model.predict(testData.map(lambda x: x.features))
+labelsAndPredictions = testData.map(lambda lp: lp.label).zip(predictions)
+
+testMSE = labelsAndPredictions.map(lambda lp: (lp[0] - lp[1])**2).sum() / float(testData.count())
+labeled_result = labelsAndPredictions.map(lambda p: Row(label=float(p[0]), predictions=float(p[1])))
+result = spark.createDataFrame(labeled_result).show(25)
+
+print('Test Mean Squared Error = ' + str(testMSE))
+
+# Save and load model
+#model.save(sc, "target/tmp/DiamondRegressionModel")
+#sameModel = RandomForestModel.load(sc, "target/tmp/DiamondRegressionModel")
